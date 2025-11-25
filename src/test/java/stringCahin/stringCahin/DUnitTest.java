e8347589912b8bde5423ce02611a16f7ad020d6b
VladislavDvorkin <159694145+DvorkinVlad@users.noreply.github.com>
2025-11-25 11:27:12 +0200

    added unit tests and config

---
+++ src/test/java/stringCahin/stringCahin/DUnitTest.java
@@ -0,0 +1,16 @@
+package stringCahin.stringCahin;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DUnitTest {
+	@Test
+    public void testDWithStub() {
+        EStub stub = new EStub();
+        D d = new D(stub);
+        String result = d.process("HEL");
+        assertEquals("HELLX", result);
+
+	}
+}
---
+++ src/test/java/stringCahin/stringCahin/EUnitTest.java
@@ -0,0 +1,16 @@
+package stringCahin.stringCahin;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class EUnitTest {
+	@Test
+    public void testE() {;
+    	E e = new E();
+        String result = e.process("HELL");
+        assertEquals("HELLO", result);
+
+	}
+
+}
